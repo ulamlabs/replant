be0fe3cd7c73078cdde4146865572e1182633e31
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-01-19 12:17:45 +0100

    RW-3: install React Query

---
+++ marketplace-app/src/modules/query/consts.ts
@@ -0,0 +1,3 @@
+export const SHORT_STALE_TIME = 15 * 1000; // 15s
+export const LONG_STALE_TIME = 20 * 60 * 1000; // 20min
+export const GC_TIME = 30 * 60 * 1000; // 30min; garbage collection time
---
+++ upload-app/src/modules/query/query-client-provider.tsx
@@ -0,0 +1,22 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import * as ReactQuery from '@tanstack/react-query';
+import { GC_TIME, SHORT_STALE_TIME } from './consts';
+
+const queryClient = new ReactQuery.QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: SHORT_STALE_TIME,
+      gcTime: GC_TIME,
+    },
+  },
+});
+
+export const QueryClientProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
+  <ReactQuery.QueryClientProvider client={queryClient}>
+    {children}
+    <ReactQueryDevtools initialIsOpen={false} position={'bottom'} />
+  </ReactQuery.QueryClientProvider>
+);
