c091e59cf62ba27d96704a2ae6fd63fdc7973fc9
klaudiaUlam <klaudia@ulam.io>
2024-01-24 10:18:23 +0100

    RW-25: clean up after rebase

--- upload-app/src/App.tsx
+++ upload-app/src/App.tsx
@@ -1,10 +1,6 @@
 import { IntlProvider } from 'modules/intl';
-import { ComponentTestpage } from 'pages';
-import { UnauthLayout } from 'modules/layout/UnauthLayout';
+import { ComponentTestpage, Home, Signup } from 'pages';
 import { QueryClientProvider } from 'modules/query';
-import { Home } from 'pages/Home';
-import { Signup } from 'pages/Signup';
-import { Test } from 'pages/Test';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
@@ -16,7 +12,6 @@ const App: React.FC = () => {
             <Route path='/testpage' element={<ComponentTestpage />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/home' element={<Home />} />
-            <Route path='/' element={<Test />} />
           </Routes>
         </BrowserRouter>
       </IntlProvider>
