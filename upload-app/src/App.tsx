25c5e0aab7464ba0b2884e3bd6e9c11db7a39150
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-02-15 14:44:03 +0100

    RW-38: remove signup without organization

--- upload-app/src/App.tsx
+++ upload-app/src/App.tsx
@@ -22,7 +22,6 @@ const App: React.FC = () => {
             <Route path='/login' element={<LoginPage />} />
             <Route path='/new-plant' element={<NewPlantPage />} />
             <Route path='/testpage' element={<ComponentTestpage />} />
-            <Route path='/signup' element={<SignupPage />} />
             <Route path='/signup-org' element={<SignupPage />} />
             <Route path='/submissions' element={<PlantSubmissionsPage />} />
             <Route path='/user' element={<UserPage />} />
