7d401779e30e14cf3f6ec401bf98c0d09a9f05a6
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-02-19 14:51:41 +0100

    RW-xx: add Not found page

--- upload-app/src/pages/index.ts
+++ upload-app/src/pages/index.ts
@@ -2,6 +2,7 @@ export * from './ComponentTestpage';
 export * from './DashboardPage';
 export * from './LoginPage';
 export * from './NewPlantPage';
+export * from './NotFoundPage';
 export * from './PlantSubmissionsPage';
 export * from './SignupPage';
 export * from './UserPage';
