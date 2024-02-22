d01ad06abd5b9e7fbaad0673074e037666a56f49
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-02-22 10:28:24 +0100

    RW-47: use icons and bigger font

--- upload-app/src/modules/user/User.tsx
+++ upload-app/src/modules/user/User.tsx
@@ -1,27 +1,24 @@
-import { useFmtMsg } from 'modules/intl';
+import { LocationIcon, PhoneIcon, UserIcon } from 'common/icons';
 import { useUser } from '.';
 
 export const User: React.FC = () => {
-  const fmtMsg = useFmtMsg();
-
   const { data: user } = useUser();
 
   return (
-    <div className='text-xs space-y-5'>
-      <div className='flex flex-col'>
-        <span className='font-bold'>{fmtMsg('login')}</span>
+    <div className='text-base space-y-5'>
+      <div className='flex gap-2 items-center'>
+        <UserIcon pathClassName='fill-gray-500' svgClassName='w-4 h-4' />
         <span>{user?.username}</span>
       </div>
-      <div className='flex flex-col'>
-        <span className='font-bold'>{fmtMsg('phoneNumber')}</span>
+      <div className='flex gap-2 items-center'>
+        <PhoneIcon svgClassName='w-4 h-4' />
         <span>{user?.phone_number}</span>
       </div>
-      <div className='flex flex-col'>
-        <span className='font-bold'>{fmtMsg('plantingOrganization')}</span>
+      <div className='flex gap-2 items-center'>
         <span>{user?.planting_organization.name}</span>
       </div>
-      <div className='flex flex-col'>
-        <span className='font-bold'>{fmtMsg('country')}</span>
+      <div className='flex gap-2 items-center'>
+        <LocationIcon svgClassName='w-4 h-4' />
         <span>{user?.country.name}</span>
       </div>
     </div>
