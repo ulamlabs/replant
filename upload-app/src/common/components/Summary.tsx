504abb4f9564ba064e3835f3a9a0a0c59acdbebe
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-02-27 12:05:49 +0100

    RW-xx: add playsInline to video element

--- upload-app/src/common/components/Summary.tsx
+++ upload-app/src/common/components/Summary.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 export const Summary: React.FC<Props> = ({ children }) => (
-  <div className='bg-gray-100 dark:bg-teal-700 px-2.5 py-5 rounded-lg flex flex-col gap-2.5'>
+  <div className='bg-gray-100 dark:bg-teal-700 p-2.5 rounded-lg flex flex-col gap-2.5'>
     {children}
   </div>
 );
