48ce7a3667ed156d7a75a855feeb021d03146757
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-03-07 14:37:03 +0100

    RW-41: style on light mode

--- upload-app/src/modules/plants/components/UploadProgressBar.tsx
+++ upload-app/src/modules/plants/components/UploadProgressBar.tsx
@@ -12,9 +12,9 @@ export const UploadProgressBar: React.FC = () => {
         <span>{fmtMsg('uploading')}</span>
         <span>{`${uploadedCount} / ${totalCount}`}</span>
       </div>
-      <div className='w-full bg-gray-500 h-1 rounded-sm'>
+      <div className='w-full bg-gray-100 dark:bg-gray-500 h-1 rounded-sm'>
         <div
-          className='bg-white h-1 rounded-sm'
+          className='bg-black dark:bg-white h-1 rounded-sm'
           style={{
             width: `${(uploadedCount / totalCount) * 100}%`,
           }}
