2310afe3ae7b9661b144aca7a8031150c8ac812b
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-03-07 15:35:48 +0100

    RW-41: add badge when waiting submissions

--- upload-app/src/modules/navigation/components/NavigationButton.tsx
+++ upload-app/src/modules/navigation/components/NavigationButton.tsx
@@ -1,25 +1,33 @@
 import clsx from 'clsx';
 import { IconProps } from 'common/icons';
-import { useNavigate, useMatch } from 'react-router-dom';
+import { useMatch, useNavigate } from 'react-router-dom';
 
 type Props = {
   Icon: React.ComponentType<IconProps>;
   path: string;
+  showBadge?: boolean;
 };
 
-export const NavigationButton: React.FC<Props> = ({ Icon, path }) => {
+export const NavigationButton: React.FC<Props> = ({
+  Icon,
+  path,
+  showBadge = false,
+}) => {
   const navigate = useNavigate();
 
   const isActive = useMatch(path);
 
   return (
-    <button onClick={() => navigate(path)}>
+    <button className='relative' onClick={() => navigate(path)}>
       <Icon
         pathClassName={clsx(
           'cursor-pointer',
           isActive && 'fill-teal-900 dark:fill-bisque-400'
         )}
       />
+      {showBadge && (
+        <div className='w-2 h-2 bg-orange-500 rounded-lg absolute top-[-0.25rem] right-[-0.25rem]' />
+      )}
     </button>
   );
 };
