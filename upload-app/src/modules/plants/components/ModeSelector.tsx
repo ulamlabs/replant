dd3ca01701d23dab295139653c0ba1ac881f8c3c
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-03-08 14:26:38 +0100

    RW-41: add badge next to Waiting tab

--- upload-app/src/modules/plants/components/ModeSelector.tsx
+++ upload-app/src/modules/plants/components/ModeSelector.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import { useFmtMsg } from 'modules/intl';
+import { useOfflineStore } from 'modules/offline';
 
 type Props = {
   offline: boolean;
@@ -9,18 +10,29 @@ type Props = {
 export const ModeSelector: React.FC<Props> = ({ offline, onToggleOffline }) => {
   const fmtMsg = useFmtMsg();
 
+  const showBadge = useOfflineStore().totalCount > 0;
+
   return (
     <div className='flex gap-5 items-start'>
       <button
-        className={clsx(!offline && 'border-b-2', 'border-black dark:border-white pb-1')}
+        className={clsx(
+          !offline && 'border-b-2',
+          'border-black dark:border-white pb-1'
+        )}
         onClick={() => onToggleOffline(false)}
       >
         {fmtMsg('submitted')}
       </button>
       <button
-        className={clsx(offline && 'border-b-2', 'border-black dark:border-white pb-1')}
+        className={clsx(
+          offline && 'border-b-2',
+          'border-black dark:border-white pb-1 flex items-center gap-1'
+        )}
         onClick={() => onToggleOffline(true)}
       >
+        {showBadge && (
+          <div className='w-2 h-2 bg-orange-500 rounded-lg' />
+        )}
         {fmtMsg('waiting')}
       </button>
     </div>
