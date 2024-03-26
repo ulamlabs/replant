e1b35abea2a78b2a13dad67c7ef8dca114b3ff09
Mikssxed <eksilonxd@gmail.com>
2024-03-26 10:58:57 +0100

    🎨RW-85: Created mobile navbar

--- marketplace-app/src/modules/navigation/components/MobileNavBar.tsx
+++ marketplace-app/src/modules/navigation/components/MobileNavBar.tsx
@@ -5,60 +5,59 @@ import { User as UserType } from 'modules/user';
 import { User } from 'modules/user/User';
 import { FC } from 'react';
 import { NavigateFunction } from 'react-router-dom';
-import { useNavToggle } from '../store';
 import NavItem from './NavItem';
 
 type Props = {
   user: UserType | undefined;
   navigate: NavigateFunction;
+  open: boolean;
+  toggleNav: () => void;
+  logOut: () => void;
 };
 
-const MobileNavBar: FC<Props> = ({ user, navigate }) => {
+const MobileNavBar: FC<Props> = ({
+  user,
+  navigate,
+  toggleNav,
+  open,
+  logOut,
+}) => {
   const fmtMsg = useFmtMsg();
-  const { isUserOpen, isNavOpen, toggleNav, closeAll } = useNavToggle();
 
   return (
-    <div className='md:hidden flex gap-4'>
-      {user && <User user={user} />}
+    <>
       <div
         onClick={toggleNav}
-        className=' w-12 h-12 bg-neutral-50 flex-col justify-center items-center gap-2 inline-flex cursor-pointer relative z-20 rounded-full dark:bg-neutral-900'
+        className=' w-7 h-7 flex-col justify-center items-center gap-2 inline-flex cursor-pointer relative  md:hidden'
       >
         <div
           className={clsx(
             'w-7 h-0.5 bg-neutral-900 dark:bg-gray-200 rounded-3xl transition-transform duration-300 transform',
-            isNavOpen && ' rotate-45 translate-y-[5px]'
+            open && ' rotate-45 translate-y-[5px]'
           )}
         />
         <div
           className={clsx(
             'w-7 h-0.5 bg-neutral-900 dark:bg-gray-200 rounded-3xl transition-transform duration-300 transform',
-            isNavOpen && ' -rotate-45 -translate-y-[5px]'
+            open && ' -rotate-45 -translate-y-[5px]'
           )}
         />
       </div>
       <div
-        onClick={closeAll}
         className={clsx(
-          'fixed h-0 w-screen left-0 top-0 bg-opacity-70 z-10 bg-zinc-400 px-[20px] sm:px-[60px] transition-all duration-300 dark:bg-neutral-700 dark:bg-opacity-70',
-          (isNavOpen || isUserOpen) && 'h-screen'
-        )}
-      ></div>
-      <div
-        className={clsx(
-          'w-[248px] max-h-0 overflow-hidden bg-neutral-50 mr-0 ml-auto transition-all duration-200 mt-[70px] rounded-3xl absolute z-20 top-0 right-5 sm:right-[60px] dark:bg-neutral-750',
-          isNavOpen && 'max-h-[455px]'
+          'w-screen max-h-0 overflow-hidden opacity-0 bg-zinc-100 transition-all duration-300 pt-0 pb-0  fixed -z-10 top-0 left-0 dark:bg-neutral-750 px-[20px] sm:px-[60px] flex flex-col justify-around',
+          open && 'max-h-screen h-screen !pt-[70px] !pb-[30px] opacity-100 '
         )}
       >
-        <ul className='flex flex-col px-6 py-5 gap-2'>
+        <ul className='flex flex-col gap-10 my-10'>
           <NavItem to='/'>{fmtMsg('home')}</NavItem>
           <NavItem to='marketplace'>{fmtMsg('marketplace')}</NavItem>
           <NavItem to='sponsors'>{fmtMsg('sponsors')}</NavItem>
           <NavItem to='planters'>{fmtMsg('planters')}</NavItem>
           <NavItem to='impact'>{fmtMsg('impact')}</NavItem>
         </ul>
-        {!user && (
-          <div className='h-[148px] px-6 py-5 bg-zinc-100 rounded-b-3xl flex flex-col gap-3 dark:bg-neutral-900'>
+        {!user ? (
+          <div className=' bg-transparent rounded-b-3xl flex flex-col gap-3'>
             <Button
               onClick={() => navigate('/signup')}
               type='primary'
@@ -74,9 +73,22 @@ const MobileNavBar: FC<Props> = ({ user, navigate }) => {
               {fmtMsg('logIn')}
             </Button>
           </div>
+        ) : (
+          <div className='flex flex-col gap-3 bg-tr'>
+            <User user={user} />
+            <Button
+              className='bg-transparent h-[59px] !text-teal-500 hover:bg-transparent dark:bg-transparent dark:!text-teal-600'
+              onClick={logOut}
+            >
+              {fmtMsg('logOut')}
+            </Button>
+          </div>
         )}
+        <p className='text-neutral-400 text-xs font-normal'>
+          Replant World Copyright © 2024
+        </p>
       </div>
-    </div>
+    </>
   );
 };
 
