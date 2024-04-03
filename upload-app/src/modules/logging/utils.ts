1e1fc3cdafd08676b009df9f2a18e601a431d4ab
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-04-03 16:00:56 +0200

    RW-91: use const instead of let

--- upload-app/src/modules/logging/utils.ts
+++ upload-app/src/modules/logging/utils.ts
@@ -4,7 +4,7 @@ export const slicedArray = <T>(array: T[], sliceSize: number): T[][] => {
   if (sliceSize <= 0 || !Number.isInteger(sliceSize)) {
     throw new Error(sliceSizeError);
   }
-  let slices: T[][] = [];
+  const slices: T[][] = [];
   for (let i = 0; i < array.length; i += sliceSize) {
     slices.push(array.slice(i, i + sliceSize));
   }
