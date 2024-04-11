0b9226886221a69ec1e0e08267f229556e5071f5
Mikssxed <eksilonxd@gmail.com>
2024-04-11 13:04:23 +0200

    🎨 RW-103: Changed nft tiles to maintain aspect ratio

--- marketplace-app/src/common/components/TreeTile.tsx
+++ marketplace-app/src/common/components/TreeTile.tsx
@@ -13,17 +13,17 @@ export function TreeTile(props: TreeDetailsProps) {
     >
       <div
         style={{ backgroundImage: `url(${props.tree.image})` }}
-        className='h-56 sm:h-96 bg-cover bg-center self-stretch rounded-lg'
+        className='w-full bg-cover bg-center self-stretch rounded-lg aspect-[0.8]'
       />
       <div className='py-4'>
-        <div className='font-medium text-sm sm:text-lg max-w-full overflow-hidden text-ellipsis text-nowrap'>
+        <div className='font-medium text-sm md:text-lg max-w-full overflow-hidden text-ellipsis text-nowrap'>
           {props.tree.botanical_name}
         </div>
-        <span className='text-neutral-400 dark:text-zinc-600 text-xs sm:text-sm font-normal'>
+        <span className='text-neutral-400 dark:text-zinc-600 text-xs md:text-sm font-normal'>
           #{props.tree.nft_id}
         </span>
         <div className='flex justify-between items-center mt-3'>
-          <span className=' text-xl sm:text-3xl font-semibold'>
+          <span className=' text-xl md:text-3xl font-semibold'>
             ${props.tree.planting_cost_usd}
           </span>
         </div>
