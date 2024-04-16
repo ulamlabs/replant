426769b81cc450fa3f7269afaad43bba99523566
Mikssxed <112573426+Mikssxed@users.noreply.github.com>
2024-04-16 12:19:02 +0200

    🎨 RW-103: Update NFT tiles (#97)
    * 🎨 RW-103: Updated nft tiles and auth layout
    
    * 🎨 RW-103: Changed nft tiles to maintain aspect ratio
    
    * 🎨 RW-103: Changed img ratio

--- marketplace-app/src/common/components/TreeTile.tsx
+++ marketplace-app/src/common/components/TreeTile.tsx
@@ -8,22 +8,23 @@ type TreeDetailsProps = {
 export function TreeTile(props: TreeDetailsProps) {
   return (
     <div
-      className='flex flex-col bg-stone-50 dark:bg-neutral-850 rounded-xl sm:rounded-3xl overflow-hidden cursor-pointer w-full transition-colors hover:bg-teal-100 dark:hover:bg-neutral-750'
+      className='flex flex-col overflow-hidden cursor-pointer w-full'
       onClick={props.onClick}
     >
-      <div
-        style={{ backgroundImage: `url(${props.tree.image})` }}
-        className='h-32 sm:h-64 bg-cover bg-center'
+      <img
+        src={props.tree.image}
+        className='w-full bg-cover bg-center rounded-lg aspect-[3/4]'
+        alt={props.tree.botanical_name}
       />
-      <div className='p-5'>
-        <div className='font-bold text-sm sm:text-lg max-w-full overflow-hidden text-ellipsis text-nowrap'>
+      <div className='py-4'>
+        <div className='font-medium text-sm md:text-lg max-w-full overflow-hidden text-ellipsis text-nowrap'>
           {props.tree.botanical_name}
         </div>
-        <span className='text-neutral-400 dark:text-zinc-600 text-xs sm:text-sm font-normal'>
+        <span className='text-neutral-400 dark:text-zinc-600 text-xs md:text-sm font-normal'>
           #{props.tree.nft_id}
         </span>
         <div className='flex justify-between items-center mt-3'>
-          <span className=' text-xl sm:text-3xl font-bold'>
+          <span className=' text-xl md:text-3xl font-semibold'>
             ${props.tree.planting_cost_usd}
           </span>
         </div>
