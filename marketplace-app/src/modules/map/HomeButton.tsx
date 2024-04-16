beb1203b6aac149a20da98eb1469d40322249325
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-04-16 17:10:01 +0200

    RW-xx: use tailwind class for transparent border background

--- marketplace-app/src/modules/map/HomeButton.tsx
+++ marketplace-app/src/modules/map/HomeButton.tsx
@@ -6,8 +6,7 @@ export const HomeButton: React.FC<Record<string, never>> = () => {
   const fmtMsg = useFmtMsg();
   return (
     <Link
-      style={{ border: '2px solid rgba(0, 0, 0, 0.2)' }}
-      className='absolute bg-clip-padding bg-white flex gap-2 hover:opacity-95 items-center left-14 px-3 py-2 rounded top-2.5 z-[1000]'
+      className='absolute bg-clip-padding bg-white border-2 border-black/20 flex gap-2 hover:opacity-95 items-center left-14 px-3 py-2 rounded top-2.5 z-[1000]'
       to='/'
     >
       <IconHome overrideColors className='fill-black' />
