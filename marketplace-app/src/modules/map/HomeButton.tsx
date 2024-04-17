40dd7a0a132aaf63e21a91dc139f00deac339773
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-04-17 09:52:40 +0200

    RW-xx: adjust style of Home navigation button on Map page

--- marketplace-app/src/modules/map/HomeButton.tsx
+++ marketplace-app/src/modules/map/HomeButton.tsx
@@ -6,11 +6,11 @@ export const HomeButton: React.FC<Record<string, never>> = () => {
   const fmtMsg = useFmtMsg();
   return (
     <Link
-      className='absolute bg-clip-padding bg-white border-2 border-black/20 flex gap-2 hover:opacity-95 items-center left-14 px-3 py-2 rounded top-2.5 z-[1000]'
+      className='absolute bg-clip-padding bg-white border-2 border-black/20 flex gap-2 hover:opacity-95 items-center left-[52px] px-1.5 py-1.5 rounded top-2.5 z-[1000]'
       to='/'
     >
-      <IconHome overrideColors className='fill-black' />
-      <span className='hidden sm:inline text-base text-black'>
+      <IconHome overrideColors className='fill-black size-4' />
+      <span className='hidden leading-none sm:inline text-black text-sm'>
         {fmtMsg('home')}
       </span>
     </Link>
